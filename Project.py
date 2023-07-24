e43b39ae44409e913cb2cf0be8a841405a3ad242
Victor Montanez <vmontanez2209@yahoo.com>
2023-07-23 19:36:22 -0700

    Add files via upload

--- Project.py
+++ Project.py
@@ -71,10 +71,11 @@ if __name__ == '__main__':
         tax_rate = get_tax_rate()
         gross_pay = get_gross_pay(hours, hourly_rate)
         tax, net_pay = calc_tax_and_netpay(hours, hourly_rate, tax_rate)
-        display_employee_info(name, hours, hourly_rate, tax_rate, tax, gross_pay, net_pay)
+        display_employee_info(name.title(), hours, hourly_rate, tax_rate, tax, gross_pay, net_pay)
+        print("Type End for totals.")
         total_employees += 1
         total_hours += hours
         total_tax += tax
         total_gross_pay += gross_pay
         total_net_pay += net_pay
-        display_total_info(total_employees, total_hours, total_tax, total_gross_pay, total_net_pay)
+    display_total_info(total_employees, total_hours, total_tax, total_gross_pay, total_net_pay)
